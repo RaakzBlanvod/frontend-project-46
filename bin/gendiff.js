7faf4c21c190723e087e82ddd738b38b1a56d266
RaakzBlanvod <icky6119@gmail.com>
2024-05-23 11:55:47 +0300

    step 3 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import path from 'path'
 const program = new Command();
 
 program
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.')
-  // .option('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format')
+.version('0.0.1', '-V, --version', 'output the version number')
+.description('Compares two configuration files and shows a difference.')
+// .option('-h, --help', 'output usage information')
+.option('-f, --format [type]', 'output format')
+.argument('filepath1')
+.argument('filepath2')
+.action((filepath1, filepath2) => {
+  const resolvedFilepath1 = path.resolve(process.cwd(), filepath1);
+  const resolvedFilepath2 = path.resolve(process.cwd(), filepath2);
+  console.log(resolvedFilepath1, resolvedFilepath2);
+})
 
-  program.parse();
+program.parse();
