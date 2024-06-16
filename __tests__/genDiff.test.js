2d97ac8c39ae3053d7f9acbf4baa05c4d95322c5
RaakzBlanvod <icky6119@gmail.com>
2024-06-16 19:21:40 +0300

    some fixing

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,20 +3,21 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
+/* eslint-disable no-restricted-syntax */
 const __filename = fileURLToPath(import.meta.url);
+/* eslint-enable no-restricted-syntax */
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const fileData = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
-	['json', 'stylish', fileData('expected_file_stylish.txt')],
-	['yml', 'stylish', fileData('expected_file_stylish.txt')],
-	['json', 'plain', fileData('expected_file_plain.txt')],
-	['yml', 'plain', fileData('expected_file_plain.txt')],
-	['json', 'json', fileData('expected_file_json.txt')],
-	['yml', 'json', fileData('expected_file_json.txt')],
-  ])('all test gendiff', (extension, format, expected) => {
-	expect(genDiff(getFixturePath(`file1.${extension}`), getFixturePath(`file2.${extension}`), format)).toEqual(expected);
+  ['json', 'stylish', fileData('expected_file_stylish.txt')],
+  ['yml', 'stylish', fileData('expected_file_stylish.txt')],
+  ['json', 'plain', fileData('expected_file_plain.txt')],
+  ['yml', 'plain', fileData('expected_file_plain.txt')],
+  ['json', 'json', fileData('expected_file_json.txt')],
+  ['yml', 'json', fileData('expected_file_json.txt')],
+])('all test gendiff', (extension, format, expected) => {
+  expect(genDiff(getFixturePath(`file1.${extension}`), getFixturePath(`file2.${extension}`), format)).toEqual(expected);
 });
-
--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,30 @@
-import globals from "globals";
+import globals from 'globals';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname, 
+recommendedConfig: pluginJs.configs.recommended });
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  ...compat.extends("airbnb"),
-  {"rules":
-  {"no-console": "off",
-    "import/extensions": ["error", "ignorePackages", {
-      "js": "always"
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  ...compat.extends('airbnb'),
+  {'rules': 
+  { 'no-console': 'off',
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'always'
     }],
-    "import/no-named-as-default": "off",
-    "import/no-named-as-default-member": "off"
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'import/no-extraneous-dependencies': ['error', {'devDependencies': true}]
   }
-  }
+  },
+  
 ];
